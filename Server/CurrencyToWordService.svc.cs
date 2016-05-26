b2b472bd81f3c0520519d63601009c3425b58f5e
AleXK <alexk.karasev@gmail.com>
2016-05-26 06:28:19 +0300

    Created template for projects: Client and Server

---
+++ Server/CurrencyToWordService.svc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Server
+{    
+    public class CurrencyToWordService : ICurrencyToWordService
+    {
+        public string CurrencyToWord(decimal currency)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Server/ICurrencyToWordService.cs
@@ -0,0 +1,19 @@
+﻿using System.ServiceModel;
+
+namespace Server
+{
+    /// <summary>
+    ///     Service for currency
+    /// </summary>
+    [ServiceContract]
+    public interface ICurrencyToWordService
+    {
+        /// <summary>
+        ///     Return word precentation of currency
+        /// </summary>
+        /// <param name="currency">Currency</param>
+        /// <returns></returns>
+        [OperationContract]
+        string CurrencyToWord(decimal currency);
+    }    
+}
